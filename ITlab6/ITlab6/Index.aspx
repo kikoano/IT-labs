6a16aabac2727bfa2a98c10264d45cf6df0ebad5
kikoano <kikoano111@yahoo.com>
2017-04-16 05:28:46 +0200

    Added ITlab6

---
+++ ITlab6/ITlab6/Index.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ITlab6.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 148px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width: 400px; margin: 0 auto;">
+            <table class="auto-style1">
+                <tr>
+                    <td style="text-align: center">
+                        <asp:ListBox ID="lstCustomers" runat="server" AutoPostBack="True" Height="220px" OnSelectedIndexChanged="lstCustomers_SelectedIndexChanged" Width="300px"></asp:ListBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="text-align: center">
+                        <asp:Label ID="lblPoraka" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="text-align: center">
+                        <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Промени" />
+&nbsp;<asp:Button ID="btnClear" runat="server" OnClick="btnClear_Click" Text="Исчисти" />
+&nbsp;<asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Внеси" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="auto-style1">
+                            <tr>
+                                <td class="auto-style2">ID</td>
+                                <td>
+                                    <asp:TextBox ID="txtID" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style2">Име на компанија</td>
+                                <td>
+                                    <asp:TextBox ID="txtCompany" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style2">Име на контакт</td>
+                                <td>
+                                    <asp:TextBox ID="txtContact" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style2">Адреса</td>
+                                <td>
+                                    <asp:TextBox ID="txtAddress" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style2">Град</td>
+                                <td>
+                                    <asp:TextBox ID="txtCity" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="auto-style2">Телефон</td>
+                                <td>
+                                    <asp:TextBox ID="txtPhone" runat="server" Width="220px"></asp:TextBox>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ITlab6/ITlab6/Index.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ITlab6
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblPoraka.Text = "";
+            if (!IsPostBack)
+                updateList();
+        }
+        private void updateList()
+        {
+            lstCustomers.Items.Clear();
+            string query = "SELECT CustomerID, CompanyName FROM Customers ORDER BY CompanyName";
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstCustomers.Items.Add(new ListItem(reader["CompanyName"].ToString(), reader["CustomerID"].ToString()));
+                }
+                reader.Close();
+
+            }
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void lstCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Customers WHERE CustomerID='"+lstCustomers.SelectedValue+"'";
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtID.Text = reader["CustomerID"].ToString();
+                    txtCompany.Text = reader["CompanyName"].ToString();
+                    txtContact.Text = reader["ContactName"].ToString();
+                    txtAddress.Text = reader["Address"].ToString();
+                    txtCity.Text = reader["City"].ToString();
+                    txtPhone.Text = reader["Phone"].ToString();
+                    reader.Close();
+                }
+
+            }
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtID.Text = "";
+            txtCompany.Text = "";
+            txtContact.Text = "";
+            txtAddress.Text = "";
+            txtCity.Text = "";
+            txtPhone.Text = "";
+            lstCustomers.SelectedIndex = -1;
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Customers SET CustomerID=@CustomerID, CompanyName=@CompanyName, ContactName=@ContactName, Address=@Address, City=@City, Phone=@Phone WHERE CustomerID=@CustomerID";
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@CustomerID", txtID.Text);
+            cmd.Parameters.AddWithValue("@CompanyName", txtCompany.Text);
+            cmd.Parameters.AddWithValue("@ContactName", txtContact.Text);
+            cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+            cmd.Parameters.AddWithValue("@City", txtCity.Text);
+            cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            updateList();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Customers(CustomerID,CompanyName,ContactName,Address,City,Phone) VALUES(@CustomerID,@CompanyName,@ContactName,@Address,@City,@Phone)";
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["Northwind"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@CustomerID", txtID.Text);
+            cmd.Parameters.AddWithValue("@CompanyName", txtCompany.Text);
+            cmd.Parameters.AddWithValue("@ContactName", txtContact.Text);
+            cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+            cmd.Parameters.AddWithValue("@City", txtCity.Text);
+            cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception err)
+            {
+                lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+            updateList();
+        }
+    }
+}
